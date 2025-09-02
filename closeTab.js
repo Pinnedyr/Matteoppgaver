8cda4af2017e8ca7c5b80d108e64124f29ec211a
Pinnedyr <168638106+Pinnedyr@users.noreply.github.com>
2025-09-02 16:35:25 +0200

    Update closeTab.js

--- closeTab.js
+++ closeTab.js
@@ -1,17 +1,16 @@
 
 document.addEventListener('keydown', function(event) {
   if (event.key === 'Alt') {
-  
-    document.body.innerHTML = '';
-    document.title = '';
 
-    window.location.href = 'about:blank';
-  }
-});
+    window.close();
 
 
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'Alt') {
-    window.close(); 
+    setTimeout(function() {
+      if (!window.closed) {
+        document.body.innerHTML = '';
+        document.title = '';
+        window.location.href = 'about:blank';
+      }
+    }, 50);
   }
 });
