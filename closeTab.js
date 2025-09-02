8417dece7b9d6f42003b5e7e1c70834c1aba0be5
Pinnedyr <168638106+Pinnedyr@users.noreply.github.com>
2025-09-02 15:12:58 +0200

    Update closeTab.js

--- closeTab.js
+++ closeTab.js
@@ -1,9 +1,5 @@
-<script>
-  document.addEventListener('keydown', function(event) {
-    // Check if the pressed key is 'T' or 't'
-    if (event.key === 'T' || event.key === 't') {
-      // Attempt to close the window
-      window.close();
-    }
-  });
-</script>
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'T' || event.key === 't') {
+    window.close(); 
+  }
+});
